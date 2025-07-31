cae0756b6eb82db47ada2a6dc5e611e7905013aa
ЛевантинаМина <valya15fenichco.com@mail.ru>
2025-07-31 22:06:46 +0800

    Signed-off-by: ЛевантинаМина <valya15fenichco.com@mail.ru>

--- text.py
+++ text.py
@@ -1,6 +1,5 @@
 #17.5.3ПРОГРАММА ВЫВОДИТ ТАБЛИЦУ УМНОЖЕНИЯ
-number = int(input())
 for i in range(1,11):
     print()
-    for j in range(1,11):
+    for j in range(1,11,1):
         print(f'{i} * {j} = {i * j}') #например: 2 * 1 = 2
