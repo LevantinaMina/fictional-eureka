c7dd362516e152e1145b97df6e1872e177f570a9
Мина <valya15fenichco.com@mail.ru>
2025-08-01 15:50:04 +0800

    Delete 17.7.1.py

--- 17.7.1.py
+++
@@ -1,7 +0,0 @@
-#17.7.1 ПРОГРАММА, КОТОРАЯ СЧИТАЕТ ПРОИЗВЕДЕНИЕ ВСЕХ НЕЧЕТНЫХ ЧИСЕЛ ОТ 1 ДО 10
-multipl = 1
-for i in range(1, 11):
-    if i % 2 == 0:
-        continue
-    multipl *= i
-print(multipl)
